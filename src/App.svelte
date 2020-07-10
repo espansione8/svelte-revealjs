6bdc811a63dfc76f514047bdfdb9df861128a2e1
espansione8 <espansione8@gmail.com>
2020-07-10 14:28:13 +0200

    retro effect first slide

--- src/App.svelte
+++ src/App.svelte
@@ -284,241 +284,36 @@
       0px 4px 0px #666, 0px 5px 0px #555, 0px 6px 0px #666, 0px 7px 0px #777,
       0px 8px 7px #999;
   }
-  /*Flickering Light*/
-  /* :root {
-    --white: #f1f1f1;
-  } */
-  div > span {
-    opacity: 0.15;
+  /*Luminance Light*/
+  #luminance {
+    background: 50% 100% / 50% 50% no-repeat
+      radial-gradient(ellipse at bottom, #fff, transparent, transparent);
+    -webkit-background-clip: text;
+    background-clip: text;
+    color: transparent;
+    animation: reveal 3000ms ease-in-out forwards 200ms,
+      glow 2500ms linear infinite 2000ms;
   }
-  #flick-div {
-    /* font-family: "Source Sans Pro", Helvetica, sans-serif;
-    letter-spacing: 0.1em; */
+  #slide1-row1 {
     font-size: 1.5em;
-    font-weight: 600;
+    font-family: "Limelight", cursive;
   }
-  #I {
-    opacity: 1;
-    animation: flickerI 2s linear reverse infinite;
-  }
-  #I::after {
-    content: "";
-    width: 150%;
-    -webkit-box-shadow: -35px 0px 60px 8px rgba(255, 255, 255, 1);
-    -moz-box-shadow: -35px 0px 60px 8px rgba(255, 255, 255, 1);
-    box-shadow: -35px 0px 60px 8px rgba(255, 255, 255, 1);
-  }
-  #L,
-  #G {
-    animation: flickerLG 2s linear reverse infinite;
-    position: relative;
-  }
-  #L::after,
-  #L::before {
-    content: " ";
-    width: 100px;
-    height: 100px;
-    background: var(--white);
-    position: absolute;
-    top: -50%;
-    left: 100%;
-    border-radius: 100%;
-    opacity: 0.05;
-    filter: blur(10px);
-  }
-  #L::after {
-    width: 200px;
-    height: 200px;
-    top: -150%;
-    left: -5%;
-    opacity: 0.1;
-    filter: blur(10px);
-  }
-  #H {
-    animation: flickerH 2s linear reverse infinite;
+  #slide1-row2 {
+    font-size: 2em;
+    font-family: "Limelight", cursive;
   }
 
-  #T {
-    animation: flickerH 2s linear reverse infinite;
-  }
-
-  @keyframes flickerI {
-    0% {
-      opacity: 0.4;
-    }
-    5% {
-      opacity: 0.5;
-    }
-    10% {
-      opacity: 0.6;
-    }
-    15% {
-      opacity: 0.85;
-    }
-    25% {
-      opacity: 0.5;
-    }
-    30% {
-      opacity: 1;
-    }
-    35% {
-      opacity: 0.1;
-    }
-    40% {
-      opacity: 0.25;
-    }
-    45% {
-      opacity: 0.5;
-    }
-    60% {
-      opacity: 1;
-    }
-    70% {
-      opacity: 0.85;
-    }
+  @keyframes reveal {
     80% {
-      opacity: 0.4;
-    }
-    90% {
-      opacity: 0.5;
+      letter-spacing: 8px;
     }
     100% {
-      opacity: 1;
+      background-size: 300% 300%;
     }
   }
-
-  @keyframes flickerLG {
-    0% {
-      opacity: 0.19;
-    }
-    5% {
-      opacity: 0.2;
-    }
-    10% {
-      opacity: 0.25;
-    }
-    15% {
-      opacity: 0.35;
-    }
-    25% {
-      opacity: 0.2;
-    }
-    30% {
-      opacity: 0.4;
-    }
-    35% {
-      opacity: 0.1;
-    }
+  @keyframes glow {
     40% {
-      opacity: 0.25;
-    }
-    45% {
-      opacity: 0.2;
-    }
-    60% {
-      opacity: 0.4;
-    }
-    70% {
-      opacity: 0.35;
-    }
-    80% {
-      opacity: 0.4;
-    }
-    90% {
-      opacity: 0.2;
-    }
-    100% {
-      opacity: 0.4;
-    }
-  }
-
-  @keyframes flickerH {
-    0% {
-      opacity: 0.15;
-    }
-    5% {
-      opacity: 0.2;
-    }
-    10% {
-      opacity: 0.12;
-    }
-    15% {
-      opacity: 0.2;
-    }
-    25% {
-      opacity: 0.15;
-    }
-    30% {
-      opacity: 0.4;
-    }
-    35% {
-      opacity: 0.05;
-    }
-    40% {
-      opacity: 0.12;
-    }
-    45% {
-      opacity: 0.15;
-    }
-    60% {
-      opacity: 0.3;
-    }
-    70% {
-      opacity: 0.2;
-    }
-    80% {
-      opacity: 0.3;
-    }
-    90% {
-      opacity: 0.18;
-    }
-    100% {
-      opacity: 0.3;
-    }
-  }
-
-  @keyframes flickerT {
-    0% {
-      opacity: 0.01;
-    }
-    5% {
-      opacity: 0.05;
-    }
-    10% {
-      opacity: 0.03;
-    }
-    15% {
-      opacity: 0.1;
-    }
-    25% {
-      opacity: 0.07;
-    }
-    30% {
-      opacity: 0.1;
-    }
-    35% {
-      opacity: 0.05;
-    }
-    40% {
-      opacity: 0.06;
-    }
-    45% {
-      opacity: 0.1;
-    }
-    60% {
-      opacity: 0;
-    }
-    70% {
-      opacity: 0.1;
-    }
-    80% {
-      opacity: 0;
-    }
-    90% {
-      opacity: 0;
-    }
-    100% {
-      opacity: 0.1;
+      text-shadow: 0 0 8px #fff;
     }
   }
 </style>
@@ -526,24 +321,13 @@
 <div class="reveal">
   <div class="slides">
     <section data-background-image="/slide1-bg.jpg">
-      <h2>SEMPRE LE STESSE IDEE</h2>
-      <!-- <h1>PROMOZIONALI?</h1> -->
-      <div id="flick-div">
-        <span id="L">P</span>
-        <span id="I">R</span>
-        <span id="G">O</span>
-        <span id="H">M</span>
-        <span id="T">O</span>
-        <span id="L">Z</span>
-        <span id="I">I</span>
-        <span id="G">O</span>
-        <span id="H">N</span>
-        <span id="T">A</span>
-        <span id="L">L</span>
-        <span id="G">I</span>
-        <span id="I">?</span>
+      <div id="luminance">
+        <!-- <h2>SEMPRE LE STESSE IDEE</h2>
+        <h1>PROMOZIONALI?</h1>
+        <h3>Allora rendi la tua memorabile..</h3> -->
+        <span id="slide1-row1">SEMPRE LE STESSE IDEE</span>
+        <span id="slide1-row2">PROMOZIONALI?</span>
       </div>
-      <!-- <h3>Allora rendi la tua memorabile..</h3> -->
     </section>
 
     <section data-background="https://i.giphy.com/90F8aUepslB84.gif">
